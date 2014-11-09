c1e236fa614a920e0cb51969af3e2b972b7294e3
Maxwell J. Koo <mjkoo90@gmail.com>
2014-11-09 16:31:41 -0500

    More moving around

---
+++ tests/accessors.cpp
@@ -0,0 +1,13 @@
+#include <gtest/gtest.h>
+
+TEST(AccessorsTest, ByteRegister) {
+    EXPECT_EQ(0, 0);
+}
+
+int
+main(int argc, char **argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
