7241fd6edb372802632835af883678e055993424
Maxwell J. Koo <mjkoo90@gmail.com>
2014-11-11 12:26:00 -0500

    Rename stuff

--- libmjkgb/src/cpu.cpp
+++ libmjkgb/src/cpu.cpp
@@ -3,7 +3,7 @@
 namespace mjkgb {
 
 Cpu::Cpu()
-  : cycles_(0),
+  : clock_(0),
     registers_()
 { }
 
@@ -14,7 +14,7 @@ uint8_t Cpu::get(ByteRegister reg) const
 
 uint16_t Cpu::get(WordRegister reg) const
 {
-    size_t index = 2 * static_cast<size_t>(reg);
+    auto index = 2 * static_cast<size_t>(reg);
     return static_cast<uint16_t>(registers_[index] << 8 | registers_[index + 1]);
 }
 
@@ -25,14 +25,14 @@ void Cpu::set(ByteRegister reg, uint8_t value)
 
 void Cpu::set(WordRegister reg, uint16_t value)
 {
-    size_t index = 2 * static_cast<size_t>(reg);
+    auto index = 2 * static_cast<size_t>(reg);
     registers_[index] = (value >> 8) & 0xff;
     registers_[index + 1] = value & 0xff;
 }
 
 void Cpu::tick()
 {
-    cycles_++;
+    clock_++;
 }
 
 }
